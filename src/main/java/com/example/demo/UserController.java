47ad98afd2841945614d1d9f7f0a40ed45d345b5
Ratthawan Sermsuwannasuk <ratthawanser@mathregit.group>
2020-08-05 13:11:57 +0700

    incomplete page

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,14 +13,19 @@ public class UserController {
     @GetMapping("/users")
     public List<UsersResponse> getAllUser() {
         List<UsersResponse> usersResponseList = new ArrayList<>();
-        usersResponseList.add(new UsersResponse(1, "User 1"));
-        usersResponseList.add(new UsersResponse(2, "User 2"));
-        usersResponseList.add(new UsersResponse(3, "User 3"));
+        usersResponseList.add(new UsersResponse(1, "User 1", 20));
+        usersResponseList.add(new UsersResponse(2, "User 2" ,10));
+        usersResponseList.add(new UsersResponse(3, "User 3" ,10));
         return usersResponseList;
     }
 
     @GetMapping("/users/{id}")
-    public UsersResponse getUserById(@PathVariable int id) {
-        return new UsersResponse(id,"User "+id);
+    public UsersResponse getUserById(@PathVariable int id, int page) {
+        return new UsersResponse(id,"User "+id, page);
     }
+
+//    @GetMapping("/users?page={page}")
+//    public UsersResponse getPagingAndItemPerPage(@PathParam("page") int page) {
+//        return new UsersResponse(page);
+//    }
 }
--- src/main/java/com/example/demo/UsersResponse.java
+++ src/main/java/com/example/demo/UsersResponse.java
@@ -3,10 +3,12 @@ package com.example.demo;
 public class UsersResponse {
     private int id;
     private String name;
+    private int page;
 
-    public UsersResponse(int id, String name) {
+    public UsersResponse(int id, String name, int page) {
         this.id = id;
         this.name = name;
+        this.page = page;
     }
 
     // PO (Plain Old Java Object: property,getter,setter)
@@ -25,5 +27,19 @@ public class UsersResponse {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public int setPage(int page) {
+        if(page == Integer.parseInt(null)) {
+            return this.page = 10;
+        }
+        else{
+            this.page = page;
+        }
+        return page;
     }
 }
